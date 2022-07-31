5296d9db5aaad09a797018c463dff02007d2c3ad
vishal5394 <vishaljadyal@gmail.com>
2022-07-31 15:47:48 +0530

    Added: Delete Contact in Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 
 public class AddressBook {
 	public static final ArrayList<ArrayList<String>> addressBook = new ArrayList<>();
+	private static String first_name;
+	private static String last_name;
+	private static String address;
+	private static String city;
+	private static String state;
+	private static String zip;
+	private static String phone_num;
+	private static String email;
 
 	
 	public void createContacts(ArrayList<String> contact) {
@@ -15,56 +23,61 @@ public class AddressBook {
 	        }
 	    }
 	}
+	
 	public void addContact() {
 	    ArrayList<String> contact = enterContactDetails();
 	    createContacts(contact);
 	}
-
+	
 	public ArrayList enterContactDetails() {
 	    ArrayList<String> contact = new ArrayList<String>();
 
 	    System.out.println("\nEnter the first name: ");
 	    Scanner sc1 = new Scanner(System.in);
-	    String first_name = sc1.next();
+	     first_name = sc1.next();
 	  //contact.add(first_name);
 
 	    System.out.println("Enter the last name: ");
 	    Scanner sc2 = new Scanner(System.in);
-	    String last_name = sc2.next();
+	    last_name = sc2.next();
 	    contact.add("Name: " +first_name +" "+ last_name);
 
 	    System.out.println("Enter the address: ");
 	    Scanner sc3 = new Scanner(System.in);
-	    String address = sc3.next();
+	     address = sc3.next();
 	    contact.add("Address: "+address);
 
 	    System.out.println("Enter the city name: ");
 	    Scanner sc4 = new Scanner(System.in);
-	    String city = sc4.next();
+	    city = sc4.next();
 	    contact.add("City: " +city);
 
 	    System.out.println("Enter the state's name: ");
 	    Scanner sc5 = new Scanner(System.in);
-	    String state = sc5.next();
+	     state = sc5.next();
 	    contact.add("State: " + state);
 
 	    System.out.println("Enter the zip: ");
 	    Scanner sc6 = new Scanner(System.in);
-	    String zip = sc6.next();
+	    zip = sc6.next();
 	    contact.add("zip: "+zip);
 
 	    System.out.println("Enter the phone number: ");
 	    Scanner sc7 = new Scanner(System.in);
-	    String phone_num = sc7.next();
+	     phone_num = sc7.next();
 	    contact.add("Contact no: "+phone_num);
 
 	    System.out.println("Enter the email ID: ");
 	    Scanner sc8 = new Scanner(System.in);
-	    String email = sc8.next();
+	     email = sc8.next();
 	    contact.add("Email: "+email);
 
 	    return contact;
 	}
+	public static void showContact() {
+		System.out.println(first_name +" "+last_name+"\n"+ address+"\n"+city+" "+state+" "+ zip+"\n"+phone_num+"\n"+email);
+	}
+
 	
 	public void addMultiplePerson() {
 	    System.out.println("\nEnter the number of persons whose details you want " + "to add to the address book\n");
@@ -73,6 +86,7 @@ public class AddressBook {
 	    for (int i = 1; i <= no_of_person; i++) {
 
 	        addContact();
+	        
 	    }
 	}
 	public int searchExistingContact() {
@@ -98,7 +112,18 @@ public class AddressBook {
 	    System.out.println("\n Found the name, Kindly enter new details ");
 	    ArrayList<String> contact = enterContactDetails();
 	      AddressBook.addressBook.set(index, contact);
-	      addContact();
+	      AddressBook.showContact();
+	}
+	public void deleteExistingContact() {
+	    System.out.println("Enter the name of the person whose details you " + "want to be deleted");
+	    Scanner sc = new Scanner(System.in);
+	    String searchPerson = sc.next();
+
+	    int index = searchExistingContact();
+
+	    AddressBook.addressBook.remove(index);
+	  
+	    AddressBook.showContact();
 	}
 	
 	public static void main(String[] args) {
@@ -108,6 +133,7 @@ public class AddressBook {
 		 details.addMultiplePerson();
 		 details.searchExistingContact();
 		 details.editExistingContact();
+		 details.deleteExistingContact();
 	}
 }
 
